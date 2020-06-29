049def212069b978259c687e6d5a562105ba2190
Ruslan Moiseev <moiseev_nvr@mail.ru>
2020-06-29 14:16:32 +0300

    add to routes.rb resources search

--- spec/helpers/search_helper_spec.rb
+++ spec/helpers/search_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe SearchControllerHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe SearchHelper, type: :helper do
+  
 end
