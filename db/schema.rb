6985842685da3c66a4dbb96ba80418aa193cfb35
Ruslan Moiseev <moiseev_nvr@mail.ru>
2020-07-04 13:56:58 +0300

    add modeles user and cart and cart_items

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,54 +10,75 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_622_131_733) do
+ActiveRecord::Schema.define(version: 2020_07_02_060636) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'brands', force: :cascade do |t|
-    t.string 'title'
-    t.string 'bytitle'
-    t.string 'img'
-    t.string 'description'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "brands", force: :cascade do |t|
+    t.string "title"
+    t.string "bytitle"
+    t.string "img"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'title'
-    t.string 'bytitle'
-    t.string 'keywords'
-    t.string 'description'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'ancestry'
-    t.index ['ancestry'], name: 'index_categories_on_ancestry'
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "cart_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'galleries', force: :cascade do |t|
-    t.integer 'product_id'
-    t.string 'img'
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'products', force: :cascade do |t|
-    t.integer 'category_id'
-    t.integer 'brand_id'
-    t.string 'title'
-    t.string 'bytitle'
-    t.text 'content'
-    t.float 'price'
-    t.float 'old_price'
-    t.integer 'status'
-    t.string 'keywords'
-    t.string 'description'
-    t.string 'img', default: 'no_image.jpg'
-    t.integer 'hit', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "bytitle"
+    t.string "keywords"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
-  create_table 'related_products', id: false, force: :cascade do |t|
-    t.integer 'product_id'
-    t.integer 'related_id'
+  create_table "galleries", force: :cascade do |t|
+    t.integer "product_id"
+    t.string "img"
   end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "brand_id"
+    t.string "title"
+    t.string "bytitle"
+    t.text "content"
+    t.float "price"
+    t.float "old_price"
+    t.integer "status"
+    t.string "keywords"
+    t.string "description"
+    t.string "img", default: "no_image.jpg"
+    t.integer "hit", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "related_products", id: false, force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "related_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
